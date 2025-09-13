5b3e65a50ff5ed6b931a6cc227b1e9c85e35227c
Krzysztof Miarka <krzeslaw42@gmail.com>
2025-09-13 19:38:18 +0200

    pong: added object class and implementation

---
+++ include/object.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <raylib.h>
+
+struct Size {
+  float x;
+  float y;
+};
+
+class Object{
+private:
+  Vector2 m_pos;
+  Size m_size;
+  Vector2 m_velocity;
+  Color m_color;
+  
+  void updatePos();
+public:
+  // Constructors/Destructors
+  Object(Vector2 pos, Size size);
+  virtual ~Object() = default;
+  Object(const Object &) = default;
+  Object(Object &&) = delete;
+  auto operator=(const Object &) -> Object & = default;
+  auto operator=(Object &&) -> Object & = delete;
+
+  // Getters
+  [[nodiscard]] auto getPosition() const -> Vector2;
+  [[nodiscard]] auto getSize() const -> Size;
+  [[nodiscard]] auto getRectangle() const -> Rectangle;
+  [[nodiscard]] auto getColor() const -> Color;
+
+  // Setters
+  void setPosition(Vector2 pos);
+  void setVelocity(Vector2 velocity);
+  void setColor(Color color);
+
+  //Update
+  void update();
+};
---
+++ src/object.cpp
@@ -0,0 +1,41 @@
+#include "../include/object.h"
+
+Object::Object(Vector2 pos, Size size)
+  : m_pos(pos), m_size(size), m_velocity({.x=0,.y=0}), m_color(WHITE){}
+
+void Object::updatePos(){
+  m_pos.x += m_velocity.x;
+  m_pos.y += m_velocity.y;
+}
+
+auto Object::getPosition() const -> Vector2{
+  return m_pos;
+}
+
+auto Object::getSize() const -> Size{
+  return m_size;
+}
+
+auto Object::getRectangle() const -> Rectangle{
+  return {m_pos.x, m_pos.y, m_size.x, m_size.y};
+}
+
+auto Object::getColor() const -> Color{
+  return m_color;
+}
+
+void Object::setPosition(Vector2 pos){
+  m_pos = pos;
+}
+
+void Object::setVelocity(Vector2 velocity){
+  m_velocity = velocity;
+}
+
+void Object::setColor(Color color){
+  m_color = color;
+}
+
+void Object::update(){
+  updatePos();
+}
