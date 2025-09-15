9b736ef70b41c11d63adfdd9547807315e04dca0
Krzysztof Miarka <krzeslaw42@gmail.com>
2025-09-15 13:54:36 +0200

    pong: working on menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -8,40 +8,41 @@ Menu::Menu()
 void Menu::update(){
   if (IsKeyPressed(KEY_UP)) {
     if (m_selectedIndex == 0) {
-      m_selectedIndex = 0;
+      m_selectedIndex = static_cast<int>(m_options.size()) -1;
     } else {
       m_selectedIndex--;
     }
   }
 
   if (IsKeyPressed(KEY_DOWN)) {
-    if (m_selectedIndex == 2) {
-      m_selectedIndex = 2;
+    if (m_selectedIndex == static_cast<int>(m_options.size()) -1) {
+      m_selectedIndex = 0;
     } else {
       m_selectedIndex++;
     }
   }
-
 }
 
 void Menu::render() const {
-    int screenWidth  = GetScreenWidth();
-    int screenHeight = GetScreenHeight();
+  int screenWidth  = GetScreenWidth();
+  int screenHeight = GetScreenHeight();
 
-    int fontSize = 40;
-    int spacing = 60;
+  int fontSize = 40;
+  int spacing = 60;
 
-    int startY = (screenHeight / 2) - ((static_cast<int>(m_options.size()) * spacing) / 2);
+  int startY = (screenHeight / 2) - ((static_cast<int>(m_options.size()) * spacing) / 2);
+    
+  DrawText("PONG", (screenWidth/2) - (MeasureText("PONG", 80)/2), 40, 80, RAYWHITE);
 
-    for (int i = 0; i < (int)m_options.size(); i++) {
-        Color color = (i == m_selectedIndex) ? YELLOW : RAYWHITE;
+  for (int i = 0; i < (int)m_options.size(); i++) {
+    Color color = (i == m_selectedIndex) ? YELLOW : RAYWHITE;
 
-        int textWidth = MeasureText(m_options[i].c_str(), fontSize);
-        int posX = (screenWidth / 2) - (textWidth / 2);
-        int posY = startY + (i * spacing);
+    int textWidth = MeasureText(m_options[i].c_str(), fontSize);
+    int posX = (screenWidth / 2) - (textWidth / 2);
+    int posY = startY + (i * spacing);
 
-        DrawText(m_options[i].c_str(), posX, posY, fontSize, color);
-    }
+    DrawText(m_options[i].c_str(), posX, posY, fontSize, color);
+  }
 }
 
 auto Menu::getSelectedIndex() const -> int {
